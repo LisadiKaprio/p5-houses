642effc65fdda06280f1ef6105ca326b8af6d02c
LisadiKaprio <lisadikaprio@gmail.com>
2022-11-11 17:23:27 +0100

    canvas responsive to window resizing; main.css

--- src/index.js
+++ src/index.js
@@ -1,4 +1,13 @@
-
+// to make it responsive
+function windowResized() {
+	resizeCanvas(window.innerWidth, window.innerHeight);
+	// if on smaller screen and portrait, don't use bg image 
+	if (window.innerHeight > window.innerWidth*1.3 && window.innerWidth < 1000) {
+	  background(10)
+	} else {
+	  // image(img, -window.innerWidth/2, -window.innerHeight/2, window.innerWidth, window.innerHeight)
+	}
+  }
 
 function setup() {
 	createCanvas(windowWidth, windowHeight);
