168fb1a7c363743a81dc867b8a0786b4eb304d2d
LisadiKaprio <lisadikaprio@gmail.com>
2022-11-15 15:37:02 +0100

    houses layer class

--- src/index.js
+++ src/index.js
@@ -32,15 +32,15 @@ class House {
 		this.xOnLayer = config.xOnLayer || 0.5; // position horizontally within layer of houses
 		this.layerY = config.layerY || 1; // position vertically 
 	}
-	drawHouse() {
+	drawHouse(layerScale, layerOnY) {
 		push();
 		// position
 		translate(
 			// in the middle
 			window.innerWidth * this.xOnLayer, 
 			// at the very bottom
-			window.innerHeight)
-		scale(this.desiredScale);
+			window.innerHeight + layerOnY)
+		scale(this.desiredScale * layerScale);
 		translate(
 			// origin in the middle horizontally of image
 			-(this.asset.width / 2), 
@@ -52,57 +52,52 @@ class House {
 	}
 }
 
+class LayerOfHouses {
+	constructor(config) {
+		this.housesArray = config.housesArray || [];
+		this.currentLayer = config.currentLayer || 1;
+		this.layerOnY = config.layerOnY || 1;
+		this.layerScale = config.layerScale || 1;
+	}
+	drawLayerOfHouses() {
+		this.housesArray.forEach(house => {
+			house.drawHouse(this.layerScale, this.layerOnY);
+		});
+	}
+}
+
 let houses;
+let mouseInputActions;
 
 function setup() {
 	wholeCanvas = createCanvas(window.innerWidth, window.innerHeight);
 	// gets done only once: search for better function other than mouseOver
 	// wholeCanvas.mouseOver(mouseInputActions);
-	houses = [
-		new House({
-			desiredScale: 0.3,
-			asset: houseAsset4,
-			xOnLayer: 0.3,
-		}),
-		new House({
-			desiredScale: 0.6,
-			asset: houseAsset3,
-			xOnLayer: 0.15,
-		}),
-		new House({
-			desiredScale: 0.45,
-			asset: houseAsset5,
-			xOnLayer: 0.7,
-		}),
-	];
+	houses = new LayerOfHouses({
+		housesArray: [
+			new House({
+				desiredScale: 0.3,
+				asset: houseAsset4,
+				xOnLayer: 0.3,
+			}),
+			new House({
+				desiredScale: 0.6,
+				asset: houseAsset3,
+				xOnLayer: 0.15,
+			}),
+			new House({
+				desiredScale: 0.45,
+				asset: houseAsset5,
+				xOnLayer: 0.7,
+			}),
+		],
+	});
 
 }
 
 function draw() {
 	background(23, 20, 29);
-	let mouseInputActions = map(mouseX, 0, window.innerWidth, 0.1, 0.5);
-	houses.forEach(house => {
-		house.drawHouse();
-	});
-}
-function placeHouse(asset, desiredScale, positionHorizontally) {
-	translate(
-		// in the middle
-		window.innerWidth * positionHorizontally, 
-		// at the very bottom
-		window.innerHeight)
-	scale(desiredScale);
-	translate(
-		// origin in the middle horizontally of image
-		-(asset.width / 2), 
-		// origin at the very bottom of image
-		-(asset.height));
-	image(
-		asset, 
-		// in the middle
-		0, 
-		// at the very bottom
-		0,
-		);
-
+	houses.layerScale = map(mouseY, 0, window.innerHeight, 0.8, 1.3);
+	houses.layerOnY = map(mouseY, 0, window.innerHeight, -(window.innerHeight / 2), (window.innerHeight / 8));
+	houses.drawLayerOfHouses();
 }
