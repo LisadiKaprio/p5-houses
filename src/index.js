516d83bbad9ae37d15f61a622d9c0b1e5944c36e
LisadiKaprio <lisadikaprio@gmail.com>
2022-11-15 16:15:01 +0100

    3d move between house layers (cursor up down)

--- src/index.js
+++ src/index.js
@@ -29,18 +29,16 @@ class House {
 	constructor(config) {
 		this.desiredScale = config.desiredScale || 0.5;
 		this.asset = config.asset || houseAsset2;
-		this.xOnLayer = config.xOnLayer || 0.5; // position horizontally within layer of houses
+		this.xOnLayer = config.xOnLayer?? 1; // position horizontally within layer of houses
 		this.layerY = config.layerY || 1; // position vertically 
 	}
 	drawHouse(layerScale, layerOnY) {
 		push();
 		// position
 		translate(
-			// in the middle
 			window.innerWidth * this.xOnLayer, 
-			// at the very bottom
-			window.innerHeight + layerOnY)
-		scale(this.desiredScale * layerScale);
+			window.innerHeight + layerOnY);
+		scale(this.desiredScale);
 		translate(
 			// origin in the middle horizontally of image
 			-(this.asset.width / 2), 
@@ -60,35 +58,110 @@ class LayerOfHouses {
 		this.layerScale = config.layerScale || 1;
 	}
 	drawLayerOfHouses() {
+		push();
+		translate((window.innerWidth / 2), 0);
+		scale(this.layerScale);
 		this.housesArray.forEach(house => {
 			house.drawHouse(this.layerScale, this.layerOnY);
 		});
+		pop();
 	}
 }
 
-let houses;
+let housesFront;
+let housesMiddle;
+let housesBack;
 let mouseInputActions;
 
 function setup() {
 	wholeCanvas = createCanvas(window.innerWidth, window.innerHeight);
 	// gets done only once: search for better function other than mouseOver
 	// wholeCanvas.mouseOver(mouseInputActions);
-	houses = new LayerOfHouses({
+	housesFront = new LayerOfHouses({
 		housesArray: [
+			new House({
+				desiredScale: 0.65,
+				asset: houseAsset4,
+				xOnLayer: -0.3,
+			}),
+			new House({
+				desiredScale: 0.6,
+				asset: houseAsset3,
+				xOnLayer: 0,
+			}),
+			new House({
+				desiredScale: 0.45,
+				asset: houseAsset5,
+				xOnLayer: 0.3,
+			}),
+			new House({
+				desiredScale: 0.3,
+				asset: houseAsset2,
+				xOnLayer: 0.5,
+			}),
+			new House({
+				desiredScale: 0.3,
+				asset: houseAsset2,
+				xOnLayer: -0.5,
+			}),
+		],
+	});
+	housesMiddle = new LayerOfHouses({
+		housesArray: [
+			new House({
+				desiredScale: 0.3,
+				asset: houseAsset4,
+				xOnLayer: -0.3,
+			}),
+			new House({
+				desiredScale: 0.4,
+				asset: houseAsset2,
+				xOnLayer: -0.2,
+			}),
+			new House({
+				desiredScale: 0.3,
+				asset: houseAsset5,
+				xOnLayer: -0.1,
+			}),
+			new House({
+				desiredScale: 0.6,
+				asset: houseAsset3,
+				xOnLayer: 0.1,
+			}),
 			new House({
 				desiredScale: 0.3,
 				asset: houseAsset4,
 				xOnLayer: 0.3,
 			}),
+
+		]
+	});
+	housesBack = new LayerOfHouses({
+		housesArray: [
+			new House({
+				desiredScale: 0.65,
+				asset: houseAsset4,
+				xOnLayer: -0.3,
+			}),
 			new House({
 				desiredScale: 0.6,
 				asset: houseAsset3,
-				xOnLayer: 0.15,
+				xOnLayer: 0,
 			}),
 			new House({
 				desiredScale: 0.45,
 				asset: houseAsset5,
-				xOnLayer: 0.7,
+				xOnLayer: 0.3,
+			}),
+			new House({
+				desiredScale: 0.3,
+				asset: houseAsset2,
+				xOnLayer: 0.5,
+			}),
+			new House({
+				desiredScale: 0.3,
+				asset: houseAsset2,
+				xOnLayer: -0.5,
 			}),
 		],
 	});
@@ -97,7 +170,16 @@ function setup() {
 
 function draw() {
 	background(23, 20, 29);
-	houses.layerScale = map(mouseY, 0, window.innerHeight, 0.8, 1.3);
-	houses.layerOnY = map(mouseY, 0, window.innerHeight, -(window.innerHeight / 2), (window.innerHeight / 8));
-	houses.drawLayerOfHouses();
+	housesBack.layerScale = map(mouseY, 0, window.innerHeight, 1, 1.2);
+	housesBack.layerOnY = map(mouseY, 0, (window.innerHeight * 2), -(window.innerHeight * 0.4), (window.innerHeight / 10));
+	
+	housesMiddle.layerScale = map(mouseY, 0, window.innerHeight, 1, 1.6);
+	housesMiddle.layerOnY = map(mouseY, 0, (window.innerHeight * 2), -(window.innerHeight * 0.4), (window.innerHeight / 6));
+	
+	housesFront.layerScale = map(mouseY, 0, window.innerHeight, 0.8, 1.9);
+	housesFront.layerOnY = map(mouseY, 0, (window.innerHeight * 2), -(window.innerHeight * 0.2), (window.innerHeight / 2));
+	
+	housesBack.drawLayerOfHouses();
+	housesMiddle.drawLayerOfHouses();
+	housesFront.drawLayerOfHouses();
 }
