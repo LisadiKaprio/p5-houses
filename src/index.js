c0e0abf98ad60ec7071af58467178fce01f9d308
LisadiKaprio <lisadikaprio@gmail.com>
2022-11-15 15:14:10 +0100

    house class

--- src/index.js
+++ src/index.js
@@ -1,17 +1,17 @@
 
-let house2;
-let house3;
-let house4;
-let house5;
+let houseAsset2;
+let houseAsset3;
+let houseAsset4;
+let houseAsset5;
 
 // some value that makes sure the assets are displayed in reasonable size within the window
 let windowRatio = window.innerWidth / (window.innerWidth / 2);
 
 function preload () {
-	house2 = loadImage('src/assets/2.png');
-	house3 = loadImage('src/assets/3.png');
-	house4 = loadImage('src/assets/4.png');
-	house5 = loadImage('src/assets/5.png');
+	houseAsset2 = loadImage('src/assets/2.png');
+	houseAsset3 = loadImage('src/assets/3.png');
+	houseAsset4 = loadImage('src/assets/4.png');
+	houseAsset5 = loadImage('src/assets/5.png');
 }
 
 // to make it responsive
@@ -25,24 +25,66 @@ function windowResized() {
 	// }
 }
 
+class House {
+	constructor(config) {
+		this.desiredScale = config.desiredScale || 0.5;
+		this.asset = config.asset || houseAsset2;
+		this.xOnLayer = config.xOnLayer || 0.5; // position horizontally within layer of houses
+		this.layerY = config.layerY || 1; // position vertically 
+	}
+	drawHouse() {
+		push();
+		// position
+		translate(
+			// in the middle
+			window.innerWidth * this.xOnLayer, 
+			// at the very bottom
+			window.innerHeight)
+		scale(this.desiredScale);
+		translate(
+			// origin in the middle horizontally of image
+			-(this.asset.width / 2), 
+			// origin at the very bottom of image
+			-(this.asset.height));
+		// render image
+		image(this.asset, 0, 0);
+		pop();
+	}
+}
+
+let houses;
+
 function setup() {
 	wholeCanvas = createCanvas(window.innerWidth, window.innerHeight);
 	// gets done only once: search for better function other than mouseOver
 	// wholeCanvas.mouseOver(mouseInputActions);
-}
+	houses = [
+		new House({
+			desiredScale: 0.3,
+			asset: houseAsset4,
+			xOnLayer: 0.3,
+		}),
+		new House({
+			desiredScale: 0.6,
+			asset: houseAsset3,
+			xOnLayer: 0.15,
+		}),
+		new House({
+			desiredScale: 0.45,
+			asset: houseAsset5,
+			xOnLayer: 0.7,
+		}),
+	];
 
+}
 
 function draw() {
 	background(23, 20, 29);
 	let mouseInputActions = map(mouseX, 0, window.innerWidth, 0.1, 0.5);
-	push();
-	placeHouse(house4, 0.5, 0.5);
-	pop();
-	push();
-	placeHouse(house2, mouseInputActions, 0.3);
-	pop();
+	houses.forEach(house => {
+		house.drawHouse();
+	});
 }
-
 function placeHouse(asset, desiredScale, positionHorizontally) {
 	translate(
 		// in the middle
