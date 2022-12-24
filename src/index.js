78f7aa58a2813766f1d0c6eab70049f6c9f337cf
LisadiKaprio <lisadikaprio@gmail.com>
2022-12-24 20:25:26 +0100

    fix isSeen condition, fix cursor, update 7 window positions

--- src/index.js
+++ src/index.js
@@ -20,9 +20,20 @@ let houseAsset3;
 let houseAsset4;
 let houseAsset5;
 let mapAsset;
-let w1IdleAsset;
 let w1HoverAsset;
 let w1SeenAsset;
+let w2HoverAsset;
+let w2SeenAsset;
+let w3HoverAsset;
+let w3SeenAsset;
+let w4HoverAsset;
+let w4SeenAsset;
+let w5HoverAsset;
+let w5SeenAsset;
+let w6HoverAsset;
+let w6SeenAsset;
+let w7HoverAsset;
+let w7SeenAsset;
 let selection1Asset;
 let selection2Asset;
 let selection3Asset;
@@ -36,9 +47,20 @@ function preload () {
 	houseAsset4 = loadImage('src/assets/4.png');
 	houseAsset5 = loadImage('src/assets/5.png');
 	mapAsset = loadImage('src/assets/map-1.jpg');
-	w1IdleAsset = loadImage('src/assets/w1-idle.jpg');
 	w1HoverAsset = loadImage('src/assets/w1-hover.jpg');
 	w1SeenAsset = loadImage('src/assets/w1-seen.jpg');
+	w2HoverAsset = loadImage('src/assets/w2-hover.jpg');
+	w2SeenAsset = loadImage('src/assets/w2-seen.jpg');
+	w3HoverAsset = loadImage('src/assets/w3-hover.jpg');
+	w3SeenAsset = loadImage('src/assets/w3-seen.jpg');
+	w4HoverAsset = loadImage('src/assets/w4-hover.jpg');
+	w4SeenAsset = loadImage('src/assets/w4-seen.jpg');
+	w5HoverAsset = loadImage('src/assets/w5-hover.jpg');
+	w5SeenAsset = loadImage('src/assets/w5-seen.jpg');
+	w6HoverAsset = loadImage('src/assets/w6-hover.jpg');
+	w6SeenAsset = loadImage('src/assets/w6-seen.jpg');
+	w7HoverAsset = loadImage('src/assets/w7-hover.jpg');
+	w7SeenAsset = loadImage('src/assets/w7-seen.jpg');
 	selection1Asset = loadImage('src/assets/selection1.png');
 	selection2Asset = loadImage('src/assets/selection2.png');
 	selection3Asset = loadImage('src/assets/selection3.png');
@@ -80,7 +102,7 @@ class Cursor {
 		noCursor();
 		noStroke();
 		fill(9, 8, 7, 50);
-		circle(mouseX+ (mouseX*0.03), mouseY+ (mouseY*0.03), cursorRadius+10)
+		circle(mouseX+ (mouseX*-0.03), mouseY+ (mouseY*-0.03), cursorRadius+10)
 		fill(9, 8, 7);
 		circle(mouseX, mouseY, cursorRadius+10);
 		if (this.currentImage) {
@@ -121,11 +143,10 @@ class Interactable {
 	draw() {
 		push();
 		this.checkMouseOver();
-		if (this.isSeen === true) {
-			console.log('render seen window');
-			this.renderedImage = image(this.assetSeen, this.positionX, this.positionY);
-		} else if (this.isHoveredOver && this.isSeen === false) {
+		if (this.isHoveredOver) {
 			this.renderedImage = image(this.asset, this.positionX, this.positionY);
+		} else if (this.isSeen) {
+			this.renderedImage = image(this.assetSeen, this.positionX, this.positionY);
 		}
 		pop();
 	}
@@ -155,22 +176,58 @@ class Map {
 					story: "<b>Who:</b> Biology Teacher, 32 <br><br><b>Currently:</b> Getting ready for the work day. <br><br><b>Mood:</b> He feels stressed after he had a heated argument with his daughter over the phone yesterday. He regrets some of the things he said, and wants to apologize to her later this evening."
 				}),
 			this.window2 = new Window({
-					sizeX: selection2Asset.width,
-					sizeY: selection2Asset.height,
-					positionX: 836,
-					positionY: 999,
-					asset: selection2Asset,
-					assetSeen: selection2Asset,
+					sizeX: w2HoverAsset.width,
+					sizeY: w2HoverAsset.height,
+					positionX: 1305,
+					positionY: 595,
+					asset: w2HoverAsset,
+					assetSeen: w2SeenAsset,
 					story: "<b>Who:</b> Engineer, 28 <br><br><b>Currently:</b> Cooking breakfast: noodles and fried sausages with tomato sauce. His vacation started this Monday. <br><br><b>Mood:</b> Calmer than usual. Lightly excited, as he remembers his plans for this evening - going to a dress shop to see his future wife try the new bride dress on."
 				}),
 			this.window3 = new Window({
-					sizeX: selection3Asset.width,
-					sizeY: selection3Asset.height,
-					positionX: 830,
-					positionY: 1097,
-					asset: selection3Asset,
-					assetSeen: selection3Asset,
+					sizeX: w3HoverAsset.width,
+					sizeY: w3HoverAsset.height,
+					positionX: 1544,
+					positionY: 100,
+					asset: w3HoverAsset,
+					assetSeen: w3SeenAsset,
 					story: "<b>Who:</b> Freelance Illustrator, 24 <br><br><b>Currently:</b> Sleeping after staying up working until 4am. <br><br><b>Mood:</b> Deep sleep."
+				}),
+			this.window4 = new Window({
+					sizeX: w4HoverAsset.width,
+					sizeY: w4HoverAsset.height,
+					positionX: 141,
+					positionY: 1189,
+					asset: w4HoverAsset,
+					assetSeen: w4SeenAsset,
+					story: "<b>Who:</b> - <br><br><b>Currently:</b> - <br><br><b>Mood:</b> -"
+				}),
+			this.window5 = new Window({
+					sizeX: w5HoverAsset.width,
+					sizeY: w5HoverAsset.height,
+					positionX: 448,
+					positionY: 1344,
+					asset: w5HoverAsset,
+					assetSeen: w5SeenAsset,
+					story: "<b>Who:</b> - <br><br><b>Currently:</b> - <br><br><b>Mood:</b> -"
+				}),
+			this.window6 = new Window({
+					sizeX: w6HoverAsset.width,
+					sizeY: w6HoverAsset.height,
+					positionX: 1229,
+					positionY: 1415,
+					asset: w6HoverAsset,
+					assetSeen: w6SeenAsset,
+					story: "<b>Who:</b> - <br><br><b>Currently:</b> - <br><br><b>Mood:</b> -"
+				}),
+			this.window7 = new Window({
+					sizeX: w7HoverAsset.width,
+					sizeY: w7HoverAsset.height,
+					positionX: 1870,
+					positionY: 1446,
+					asset: w7HoverAsset,
+					assetSeen: w7SeenAsset,
+					story: "<b>Who:</b> - <br><br><b>Currently:</b> - <br><br><b>Mood:</b> -"
 				}),
 		]
 	};
@@ -212,6 +269,7 @@ function mouseClicked() {
 			window.isSeen = true;
 		}
 	})
+	return false;
 }
 
 let scrollVelocity = 1.2;
@@ -271,13 +329,13 @@ function setup() {
 	cameraBoundsLeft = 0;
 	cameraBoundsTop = 0;
 	setHorizontalCameraBounds();
-}
-
-function draw() {
 
 	background(9, 8, 7);
 
 	map.setup();
+}
+
+function draw() {
 
 	// smooth drag and pan
 	difX = targetCameraPositionX - cameraPositionX;
